f4112b59c87e73bfe78685e1cf2d401815d5ad43
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-07-21 09:42:57 +0300

    GetAllMovies api yazıldı

--- Domain/FilmApi.Domain/Entities/AppUser.cs
+++ Domain/FilmApi.Domain/Entities/AppUser.cs
@@ -4,7 +4,7 @@ namespace FilmApi.Domain.Entities
     public class AppUser : IdentityUser<Guid>
     {
         public string FirstName { get; set; } = string.Empty;
-        public string? LastName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string FullName => string.Join(" ", FirstName, LastName);
         public DateTime RegistrationDate { get; set; }
 
---
+++ WebApi/FilmApi.WebApi/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using FilmApi.Application.Features.Movies.Queries.GetAllMovies;
+using FilmApi.WebApi.Abstraction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmApi.WebApi.Controllers
+{
+    public class MovieController : ApiController
+    {
+        private readonly IMediator mediator;
+
+        public MovieController(IMediator mediator) : base(mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var response = await mediator.Send(new GetAllMoviesQueryRequest());
+            return Ok(response);
+        }
+    }
+}
