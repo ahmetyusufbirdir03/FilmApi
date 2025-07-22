22697320d267f4cca3f479e06b1f62e1a44c20e1
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-07-22 11:10:10 +0300

    Error Response sınıfı ile hata mesajları düzeltildi.

---
+++ Application/FilmApi.Application/Errors/ErrorResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace FilmApi.Application.Errors;
+
+public class ErrorResponse<T>
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public int StatusCode { get; set; }
+    public T? Data { get; set; }
+
+    public static ErrorResponse<T> Success(T? data = default, string? message = null, int statusCode = 200)
+        => new()
+        {
+            IsSuccess = true,
+            Data = data,
+            Message = message,  
+            StatusCode = statusCode
+        };
+
+    public static ErrorResponse<T> Failure(string message, int statusCode = 400)
+        => new()
+        {
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode
+        };
+
+
+}
--- Application/FilmApi.Application/Features/Movies/Commands/DeleteMovie/DeleteMovieCommandRequest.cs
+++ Application/FilmApi.Application/Features/Movies/Commands/DeleteMovie/DeleteMovieCommandRequest.cs
@@ -1,39 +1,9 @@
-﻿using FilmApi.Application.Interfaces.UnitOfWork;
-using FilmApi.Domain.Entities;
+﻿using FilmApi.Application.Errors;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FilmApi.Application.Features.Movies.Commands.DeleteMovie;
 
-public class DeleteMovieCommandRequest : IRequest<string>
+public class DeleteMovieCommandRequest : IRequest<ErrorResponse<string>>
 {
     public Guid Id { get; set; }  // Silinecek filmin ID'si
-}
-
-public class DeleteMovieCommandHandler : IRequestHandler<DeleteMovieCommandRequest, string>
-{
-    private readonly IUnitOfWork unitOfWork;
-
-    public DeleteMovieCommandHandler(IUnitOfWork unitOfWork)
-    {
-        this.unitOfWork = unitOfWork;
-    }
-
-    public async Task<string> Handle(DeleteMovieCommandRequest request, CancellationToken cancellationToken)
-    {
-        Movie? movie = await unitOfWork.GetGenericRepository<Movie>().GetByIdAsync(request.Id);
-        if(movie is null)
-        {
-            return "Movie could not be found!";
-        }
-
-        await unitOfWork.GetGenericRepository<Movie>().DeleteAsync(movie);
-        await unitOfWork.SaveChangesAsync();
-
-        return "Movie is deleted successfully!";
-    }
 }
